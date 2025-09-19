7adf2225488c0e9a38552721b37845ebc829a492
EncangCutbray <cabe2cabean214@gmail.com>
2025-09-19 20:51:23 +0700

    feat: Update GetAllCourierRequest to use string pointers for pagination and radius fields

--- domain/courier/handler/request/get_all_courier_request.go
+++ domain/courier/handler/request/get_all_courier_request.go
@@ -10,9 +10,9 @@ type GetAllCourierRequest struct {
 	Email     *string `form:"email" binding:"omitempty,min=1"`
 	Longitude *string `form:"longitude" binding:"omitempty,min=1,longitude"`
 	Latitude  *string `form:"latitude" binding:"omitempty,min=1,latitude"`
-	Radius    *string `form:"radius" binding:"omitempty"`
-	PerPage   *int    `form:"per_page" binding:"omitempty,gte=1"`
-	Page      *int    `form:"page" binding:"omitempty,gte=1"`
+	Radius    *string `form:"radius" binding:"omitempty,numeric,gte=0"`
+	PerPage   *string `form:"per_page" binding:"omitempty,numeric,gte=1"`
+	Page      *string `form:"page" binding:"omitempty,numeric,gte=1"`
 	SortBy    *string `form:"sort_by" binding:"omitempty,oneof=id distance_in_meters"`
 	OrderBy   *string `form:"order_by" binding:"omitempty,oneof=ASC DESC asc desc"`
 }
@@ -24,8 +24,8 @@ func (r *GetAllCourierRequest) ToEntity() *entity.SearchCourier {
 		Longitude: utils.ParseFloat64(r.Longitude),
 		Latitude:  utils.ParseFloat64(r.Latitude),
 		Radius:    utils.DerefOrDefault(utils.ParseIntPointer(r.Radius), 0),
-		Page:      utils.DerefOrDefault(r.Page, 1),
-		PerPage:   utils.DerefOrDefault(r.PerPage, 10),
+		Page:      utils.DerefOrDefault(utils.ParseIntPointer(r.Page), 1),
+		PerPage:   utils.DerefOrDefault(utils.ParseIntPointer(r.PerPage), 10),
 		SortBy:    utils.DerefOrDefault(r.SortBy, "id"),
 		OrderBy:   utils.DerefOrDefault(r.OrderBy, "asc"),
 	}
